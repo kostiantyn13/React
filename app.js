c28dd17ebaf791b8ee21d2e869e730109c9c3ec8
Kostiantyn <kos.kondratenko@gmail.com>
2018-01-27 02:24:28 +0200

    1.1

---
+++ app.js
@@ -0,0 +1,4 @@
+var element = React.createElement("h1", null, "Привет React"); //от этого метода получаем элемент
+//React - обьект
+//метод принимает 3 елемента(название дом элемента, свойства, содержимое создаваемого элемента)
+ReactDOM.render(element, document.getElementById("root"));
