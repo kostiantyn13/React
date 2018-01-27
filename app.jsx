9e5e52f42797d102c158ff1a2c2dce541c76a0be
Kostiantyn <kos.kondratenko@gmail.com>
2018-01-27 20:52:55 +0200

    1.2

--- app.jsx
+++ app.jsx
@@ -1,10 +1,47 @@
+//React создает виртуальный дом-элемент(обьектом)
 //код JSX НОРМАЛЬНЫЕ ЛЮДИ пишут в файлах с расширением *.jsx  Это не обязательно, можно и js
 //этот файл транспилируется в JavaScript
-var jsx = (
+/*var jsx = (
   <div className="container">
     <img src="favicon.ico" />
     <h1>React</h1>
     <p>Библиотека для создания пользовательских интерфейсов</p>
   </div>
 );
-ReactDOM.render(jsx, document.getElementById("root"));
+var hhh = React.createElement("div", null, jsx);
+console.log(hhh); //всегда вернет обьект с кучей свойств
+var dom = ReactDOM.render(jsx, document.getElementById("root"));
+console.log(dom); //всегда вернет готовый (виртуальный) дом
+//COMPONENT
+//На базовом уровне это функция, которая возвращает один или несколько
+//реакт элементов
+*/
+function Guru(props) {
+  // это функциональний вид компонента
+  // так как просто возвращает JSX
+  // можно передать аргументы - СВОЙСТВА - property("props")
+  //    можно использовать только выражения(напр. цикл - это не выражение, ибо ничего не возвращает)
+  return (
+    <div>
+      <img src={props.imgUrl} />
+      <h1>{props.title}</h1>
+      <p>{props.subtitle}</p>
+    </div>
+  );
+}
+ReactDOM.render(
+  <div>
+    <Guru
+      imgUrl="favicon.ico"
+      title="React"
+      subtitle="Библиотека для создания пользовательских интерфейсов"
+    />
+    <Guru
+      imgUrl="img/angular.png"
+      title="Angular 2"
+      subtitle="Один фреймворк"
+    />
+  </div>,
+  document.getElementById("root")
+);
+//есть сложные компоненты
